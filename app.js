5608063f4719507478e9e04a5f1643ab62cab76f
Ilya Pyankov <beorgerer64@gmail.com>
2021-10-21 05:47:25 +0500

    db production mode

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const cors = require('./middlewares/cors');
 const limiter = require('./middlewares/limiter');
 require('dotenv').config();
 
-const { PORT = 4000 } = process.env;
+const { PORT = 4000, DB_URL, NODE_ENV } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/moviesdb');
+mongoose.connect(NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/freedb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
